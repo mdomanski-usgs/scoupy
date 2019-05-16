b3201f25e9ca277e4adc26329d9aca9a74980e0d
Marian Domanski <mdomanski@usgs.gov>
2019-05-16 10:26:56 -0500

    add major classes to package import

---
+++ scoupy/__init__.py
@@ -0,0 +1,4 @@
+from scoupy.acousticsample import AcousticSample
+from scoupy.sedimentsample import SedimentSample
+from scoupy.sedimentsizedistribution import SedimentSizeDistribution
+from scoupy.water import WaterProperties
