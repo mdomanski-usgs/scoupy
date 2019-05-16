fd1afaf4a994d7b47b98ff5e4ba79a138519df7c
Marian Domanski <mdomanski@usgs.gov>
2019-05-16 10:50:27 -0500

    add sphinx build to setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,38 @@
-from distutils.core import setup
+from setuptools import setup
+
+from sphinx.setup_command import BuildDoc
+
+name = 'scoupy'
+version = '0.0'
+release = '0.0.0'
+cmdclass = {'build_sphinx': BuildDoc}
+docs_source = 'docs/'
+docs_build_dir = 'docs/_build'
+docs_builder = 'html'
 
 setup(
-    name='scoupy',
-    version='0.0.1dev1',
+    name=name,
+    version=release,
     packages=['scoupy'],
     url='https://github.com/mdomanski-usgs/scoupy',
     license='CC0 1.0',
     author='Marian Domanski',
     author_email='mdomanski@usgs.gov',
-    description='Hydroacoustic sediment analysis',
+    description='Sediment acoustic analysis in Python',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python 3.7'
     ],
     python_requires='>=3',
-    install_requires=['numpy', 'scipy']
+    install_requires=['numpy', 'scipy'],
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', release),
+            'source_dir': ('setup.py', docs_source),
+            'build_dir': ('setup.py', docs_build_dir),
+            'builder': ('setup.py', docs_builder)}
+    }
 )
